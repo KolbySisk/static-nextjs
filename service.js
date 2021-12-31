6ead7b8588fbadf1ba07a9073bcaca538219d443
Kolby Sisk <kolby.sisk@gmail.com>
2021-12-31 11:26:40 -0600

    refactor: update rtk query to use graphql

--- service.js
+++ service.js
@@ -1,17 +1,46 @@
 // Need to use the React-specific entry point to import createApi
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi } from "@reduxjs/toolkit/query/react";
+import { graphqlRequestBaseQuery } from "@rtk-query/graphql-request-base-query";
 
 // Define a service using a base URL and expected endpoints
-export const pokemonApi = createApi({
-  reducerPath: "pokemonApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }),
+export const api = createApi({
+  reducerPath: "api",
+  baseQuery: graphqlRequestBaseQuery({
+    baseUrl: "https://graphqlzero.almansi.me/api",
+  }),
   endpoints: (builder) => ({
-    getPokemonByName: builder.query({
-      query: (name) => `pokemon/${name}`,
+    getPosts: builder.query({
+      query: () => ({
+        body: gql`
+          query {
+            posts {
+              data {
+                id
+                title
+              }
+            }
+          }
+        `,
+      }),
+      transformResponse: (response) => response.posts.data,
+    }),
+    getPost: builder.query({
+      query: (id) => ({
+        body: gql`
+        query {
+          post(id: ${id}) {
+            id
+            title
+            body
+          }
+        }
+        `,
+      }),
+      transformResponse: (response) => response.post,
     }),
   }),
 });
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetPokemonByNameQuery } = pokemonApi;
+export const { useGetPostQuery, useGetPostsQuery } = api;
--- store.js
+++ store.js
@@ -1,17 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-// Or from '@reduxjs/toolkit/query/react'
 import { setupListeners } from "@reduxjs/toolkit/query";
-import { pokemonApi } from "./service";
+import { api } from "./service";
 
 export const store = configureStore({
   reducer: {
     // Add the generated reducer as a specific top-level slice
-    [pokemonApi.reducerPath]: pokemonApi.reducer,
+    [api.reducerPath]: api.reducer,
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(pokemonApi.middleware),
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
